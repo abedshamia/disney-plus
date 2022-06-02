915a494077a8ba571297eaa84879f1d6d3cce9bb
abedshamia <ab.shamia96@gmail.com>
2022-06-02 21:41:51 +0300

    Add Image Slider to Home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import styled from 'styled-components'
+import ImgSlider from './ImgSlider'
 
 const Container = styled.main `
     min-height: calc(100vh - 70px);
     padding: 0 calc(3.5vw + 5px);
     position: relative;
+    overflow-x: hidden;
+
     &:before {
         background: url('/images/home-background.png') center center / cover no-repeat fixed;
         content: '';
@@ -13,11 +16,14 @@ const Container = styled.main `
         left: 0;
         right: 0;
         bottom: 0;
+        z-index: -1;
     }
 `
 const Home = () => {
   return (
-    <Container>Home</Container>
+    <Container>
+        <ImgSlider />
+    </Container>
   )
 }
 
